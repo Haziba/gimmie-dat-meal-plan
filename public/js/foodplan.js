538bbf4fa477ef736cd652f80b65caa381041bba
Harry Boyes <harry.boyes@gmail.com>
2017-01-31 19:04:23 +0000

    Calculate ingredients

--- public/js/foodplan.js
+++ public/js/foodplan.js
@@ -10,6 +10,7 @@ $(function(){
   ];
 
   var mealArray = [];
+  var ingredients = {};
   
   for(var meal in meals){
     meals[meal].name = meal;
@@ -19,7 +20,21 @@ $(function(){
 
   for(var i = 0; i < week.length; i++){
     week[i].meal = mealArray[Math.floor(Math.random() * mealArray.length)];
-  }
 
+    $("#week").append($("<tr/>")
+          .append($("<td/>")
+            .text(week[i].day))
+          .append($("<td/>")
+            .text(week[i].meal.name)));
+
+    for(var j = 0; j < week[i].meal.ingredients.length; j++){
+      var ingredient = week[i].meal.ingredients[j];
+      if(!ingredients[ingredient.type]){
+        ingredients[ingredient.type] = 0;
+      }
+      ingredients[ingredient.type] += ingredient.quantity:
+    }
+  }
+  
   console.log(week);
 });
