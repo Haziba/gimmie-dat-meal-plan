7bf7049597f9909a00906c49b073a7d6c949a752
Harry Boyes <harry.boyes@gmail.com>
2017-01-31 17:26:02 +0000

    Create ingredients 'enum'

---
+++ public/js/ingredients.js
@@ -0,0 +1,36 @@
+var ingredient = {
+  /* Veg */
+  FROZEN_ONIONS: "fronions",
+  FROZEN_PEPPERS: "freppers",
+  COURGETTE: "courgette",
+  AUBERGINE: "aubergine",
+
+  /* Bread */
+  NAANS: "naans",
+  PITTAS: "pittas",
+  WRAPS: "wraps",
+
+  /* Carbs */
+  PASTA: "pasta",
+  RICE: "rice",
+  RISOTTO_RICE: "risotice",
+
+  /* Protein */
+  QUORN_MINCE: "fakince",
+  QUORN_CHICKEN: "ficken",
+  TOFU_BITS: "tofits",
+
+  /* Cheese */
+  HALLOUMI: "halloumi",
+  MOZZERELLA: "mozzerella",
+  PANEER: "paneer",
+
+  /* Spices mixes sauces */
+  CURRY_SAUCE: "currysauce",
+  FAJITA_MIX: "fajitamix",
+  CHILLI_SAUCE: "chillisauce",
+  PASTA_SAUCE: "pastasauce",
+
+  /* Currently uncategorised extras */
+  WHITE_WINE: "whine"
+};
