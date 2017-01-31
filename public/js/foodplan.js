cfb4aeb41f4a18427938c137ccfa70a7154b1ee3
Harry Boyes <harry.boyes@gmail.com>
2017-01-31 18:00:29 +0000

    Distribuet meals

--- public/js/foodplan.js
+++ public/js/foodplan.js
@@ -1,3 +1,25 @@
 $(function(){
-  console.log("Cool");
+  var week = [
+   { day: "Monday" },
+   { day: "Tuesday" },
+   { day: "Wednesday" },
+   { day: "Thursday" },
+   { day: "Friday" },
+   { day: "Saturday" },
+   { day: "Sunday" }
+  ];
+
+  var mealArray = [];
+  
+  for(var meal in meals){
+    meals[meal].name = meal;
+
+    mealArray.push(meals[meal]);
+  }
+
+  for(var i = 0; i < week.length; i++){
+    week[i].meal = mealArray[Math.floor(Math.random() * mealArray.length)];
+  }
+
+  console.log(week);
 });
